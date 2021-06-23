f2527be5b9c349e59aca2abaf1187d7e27b61cb9
Rishabh Chawla <rishabh31121999@gmail.com>
2021-06-23 15:02:23 +0530

    chore: use traverse and parse

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-import * as babel from "@babel/core";
+import * as parser from "@babel/parser";
+import traverse from "@babel/traverse";
 import { readFileSync } from "fs";
 import { resolve } from "path";
 
@@ -6,19 +7,17 @@ const code = readFileSync(resolve(__dirname, "../input/code.txt")).toString();
 const staticImport = [];
 const dynamicImport = [];
 
-babel.transformSync(code, {
-  plugins: [() => ({
-    visitor: {
-      ImportDeclaration(path) {
-        staticImport.push(path.node.source.value);
-      },
-      CallExpression(path) {
-        if (path.node.callee.type === "Import") {
-          dynamicImport.push(path.node.arguments[0].value);
-        }
-      }
+const ast = parser.parse(code, { sourceType: "module" });
+
+traverse(ast, {
+  ImportDeclaration(path) {
+    staticImport.push(path.node.source.value);
+  },
+  CallExpression(path) {
+    if (path.node.callee.type === "Import") {
+      dynamicImport.push(path.node.arguments[0].value);
     }
-  })],
+  },
 });
 
 console.log(staticImport);
