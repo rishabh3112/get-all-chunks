52fc610ac9ed02e8fc34bc4b8affc55bd3b4b22d
Rishabh Chawla <rishabh31121999@gmail.com>
2021-06-24 18:17:58 +0530

    feat: format CLI

--- src/index.js
+++ src/index.js
@@ -2,29 +2,42 @@
 import createApp from "./server";
 import { program as argParser } from "commander";
 import { resolve } from "path";
+import { cyanBright, greenBright, bold, gray } from "chalk";
 
 const run = () => {
-    argParser.option('-r, --root <root>', 'path to source directory of project', './');
-    argParser.parse();
-    
-    const args = argParser.opts();
-    if (args.root === undefined) {
-        args.root = "./";
-    }
-    
-    const resolvedRoot = resolve(process.cwd(), args.root);
-    
-    const app = createApp(resolvedRoot);
-    
-    app.listen(3000, () => {
-        const baseRoute = "http://localhost:3000";
-        console.clear();
-        console.log('\n\tPick Chunks\n');
-        console.log(`Root : ${resolvedRoot}`);
-        console.log(`Routes :`);
-        console.log(` - GET  ${baseRoute}/files : Lists all JS/TS/TSX files in root`);
-        console.log(` - POST ${baseRoute}/chunks : Gives list of chunks and dependency tree for given file\n`);
-    })
-}
+  argParser.option(
+    "-r, --root <root>",
+    "path to source directory of project",
+    "./"
+  );
+  argParser.parse();
+
+  const args = argParser.opts();
+  if (args.root === undefined) {
+    args.root = "./";
+  }
+
+  const resolvedRoot = resolve(process.cwd(), args.root);
+
+  const app = createApp(resolvedRoot);
+
+  app.listen(3000, () => {
+    const baseRoute = "http://localhost:3000";
+    console.clear();
+    console.log(`\n${bold("Pick Chunks")}\n`);
+    console.log(`${greenBright("Root")} : ${resolvedRoot}`);
+    console.log(`${greenBright("Routes")} :`);
+    console.log(
+      ` - ${bold("GET")}  ${cyanBright(`${baseRoute}/files`)} :\n   ${gray(
+        "Lists all JS/TS/TSX files in root"
+      )}`
+    );
+    console.log(
+      ` - ${bold("POST")} ${cyanBright(`${baseRoute}/chunks`)} :\n   ${gray(
+        "Gives list of chunks and dependency tree for given file"
+      )}\n`
+    );
+  });
+};
 
 run();
