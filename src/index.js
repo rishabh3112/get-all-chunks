8c156453838a55de9e16c6ad71e0cdef974c9124
Rishabh Chawla <rishabh31121999@gmail.com>
2021-06-24 13:36:16 +0530

    feat: implement chunks route

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 import express from "express";
 import { getAllFiles } from "./utils/getAllFiles";
+import { getAllChunks } from "./utils/getAllChunks";
 
 const app = express();
 
 app.use(express.json());
 
-app.post('/chunks', (req, res) => {
-    // TODO
+app.post('/chunks', async (req, res) => {
+    const body = req.body;
+    const tree = await getAllChunks(body.path);
+    const response = JSON.stringify({
+        tree,
+        chunks: tree.chunks,
+    }, (_, value) => {
+        // Stringify Set
+        if (typeof value === 'object' && value instanceof Set) {
+            return [...value];
+        }
+        return value;        
+    });
+    res.json(JSON.parse(response));
 });
 
-app.get('/files', (req, res) => {
-    const files = getAllFiles();
+app.get('/files', async (req, res) => {
+    const files = await getAllFiles();
     res.send({
         cwd: process.cwd(),
         files,
