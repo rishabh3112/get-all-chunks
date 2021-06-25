9d0832cd12f86fc99e15c614085a13e742f3ddaf
Rishabh Chawla <rishabh31121999@gmail.com>
2021-06-25 11:28:09 +0530

    feat: implement pagination for files

--- src/server.js
+++ src/server.js
@@ -3,37 +3,52 @@ import { getAllFiles } from "./utils/getAllFiles";
 import { getAllChunks, clearStore } from "./utils/getAllChunks";
 
 const createApp = (root) => {
-    const app = express();
-    app.use(express.json());
+  const app = express();
 
-    app.post('/chunks', async (req, res) => {
-        const body = req.body;
-        const tree = await getAllChunks(body.path).then((tree) => {
-            clearStore();
-            return tree;
-        });
-        const response = JSON.stringify({
-            tree,
-            chunks: tree.chunks,
-        }, (_, value) => {
-            // Stringify Set
-            if (typeof value === 'object' && value instanceof Set) {
-                return [...value];
-            }
-            return value;        
-        });
-        res.json(JSON.parse(response));
+  app.use(express.json());
+
+  app.post("/chunks", async (req, res) => {
+    const body = req.body;
+    const tree = await getAllChunks(body.path).then((tree) => {
+      clearStore();
+      return tree;
+    });
+    const response = JSON.stringify(
+      {
+        tree,
+        chunks: tree.chunks,
+      },
+      (_, value) => {
+        // Stringify Set
+        if (typeof value === "object" && value instanceof Set) {
+          return [...value];
+        }
+        return value;
+      }
+    );
+    res.json(JSON.parse(response));
+  });
+
+  app.get("/files", async (_, res) => {
+    const files = await getAllFiles(root);
+    const filesPerPage = 10;
+    const pages = {};
+
+    files.forEach((file, index) => {
+      const page = parseInt(index / filesPerPage);
+      if (pages[page] === undefined) {
+        pages[page] = [];
+      }
+      pages[page].push(file);
     });
 
-    app.get('/files', async (_, res) => {
-        const files = await getAllFiles(root);
-        res.send({
-            directory: root,
-            files,
-        });
+    res.send({
+      directory: root,
+      pages,
     });
+  });
 
-    return app;
-}
+  return app;
+};
 
 export default createApp;
